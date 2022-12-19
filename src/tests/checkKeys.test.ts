54af17d44ede2bfec7febe0fb7fa063a2aa1ab40
Mitya <partyzan327@gmail.com>
2022-12-20 00:08:13 +0300

    refactor: test checkKeys

--- src/tests/checkKeys.test.ts
+++ src/tests/checkKeys.test.ts
@@ -1,24 +1,24 @@
-// import { checkKeys } from '.././helpers/checkKeys';
-
-// describe('checkKeys:', () => {
-
-//     it('Should be true', () => {
-//         const keys = ['username', 'age', 'hobbies'];
-//         const target = { id: 'test',
-//             username: ''
-//             age: number;
-//             hobbies: string[] | [] };
-
-//         console.log(checkKeys(target, keys));
+import { checkKeys } from '../helpers/checkKeys';
 
 
-//         expect(checkKeys(target, keys)).toBeTruthy();
-//     });
+describe('checkKeys:', () => {
 
-//     // it('Should be false', () => {
-//     //     const keys = ['id', 'userName', 'age', 'hobbies'];
-//     //     const target = { id: 'test', username: 'a', age: 24, hobbies: ['1', '2'] };
+    it('Should be true', () => {
+        const keys = ['id', 'username', 'age', 'hobbies'];
+        const target = {
+            id: 'test',
+            username: 'Amigo',
+            age: 24,
+            hobbies: ['hob1', 'hob2']
+        };
 
-//     //     expect(checkKeys(target, keys)).toBeFalsy();
-//     // });
-// });
+        expect(checkKeys(target, keys)).toBeTruthy();
+    });
+
+    it('Should be false', () => {
+        const keys = ['id', 'userName', 'age', 'hobbies'];
+        const target = { id: 'test', username: 'a', age: 24, hobbies: ['1', '2'] };
+
+        expect(checkKeys(target, keys)).toBeFalsy();
+    });
+});
